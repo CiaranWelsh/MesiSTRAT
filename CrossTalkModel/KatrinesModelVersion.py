2defd6a2156dd2fd917ac14d1e6114c83a42ca6a
CiaranWelsh <c.welsh2@newcastle.ac.uk>
2018-11-27 22:22:26 +0000

    removed functions. Returned to writing rate laws myself. More reliable for some reason.

--- CrossTalkModel/KatrinesModelVersion.py
+++ CrossTalkModel/KatrinesModelVersion.py
@@ -11,61 +11,6 @@ seaborn.set_style('white')
 seaborn.set_context('talk', font_scale=1)
 
 
-def functions_antstr():
-    """
-    Return a string containing functions that can be prepended onto
-    an antimony model so that they can be used in rate laws. That's the theory
-    anyway, in practice there is an issue with the functions feature in antimony
-    that I havent worked out yet.
-    :return:
-    """
-    return """
-    function Hill(kcat, E, S, Shalve, h)
-        kcat*E*(S/Shalve)^h / (1 + (S/Shalve)^h)
-    end
-    
-    function HillWithKcat(kcat, E, S, km, h)
-        kcat*E*S^h / (km^h + S^h)
-    end
-    
-    function MM( km, S, Vmax)
-      Vmax*S / (km + S)
-    end 
-    
-    function MMWithKcat( km, S, E, kcat)
-      kcat*E*S / (km + S)
-    end
-    
-    function NonCompetitiveInhibition( km, Ki, I, E, V, n)
-      V*E/((1 + (I/Ki)^n)*(km + E));
-    end
-    
-    function MA1Mod(k, S, M)
-        k*M*S
-    end
-    
-    function MA2Mod(k, S1, S2, M)
-        k*M*S1*S2
-    end
-    
-    function MA0(k)
-      k
-    end    
-    
-    function MA1(k, S)
-        k*S
-    end
-    
-    function MA2(k, S1, S2)
-        k*S1*S2
-    end
-    
-    function MA3(k, S1, S2, S3)
-        k*S1*S2*S3
-    end
-    
-    """  # 35 lines
-
 
 def cross_talk_model_antstr():
     """
@@ -103,50 +48,45 @@ def cross_talk_model_antstr():
         var Everolimus       in Cell
     
         
-        // TGFb module
-        // Note: TGFb is consumed in this process
-        R1:                 => TGFbR            ; Cell * MA0(       kTGFbRProd                       );
-        R2: TGFbR           =>                  ; Cell * MA1(       kTGFbRDeg,      TGFbR            );     
-        //R3: TGFbR + TGFb    => TGFbR_a          ; Cell * MA2(       kTGFbOn,        TGFbR,  TGFb     );
-        R3: TGFbR           => TGFbR_a          ; Cell *   HillWithKcat(kTGFbOn_kcat, TGFb, TGFbR, kTGFbOn_halve, kTGFbOn_h);
-        R4: TGFbR_a         => TGFbR     ; Cell * MA1(       kTGFbOff,       TGFbR_a          );
-        R5: Smad2           => pSmad2           ; Cell * MA1Mod(    kSmad2Phos,     Smad2,  TGFbR_a  );
-        R6: pSmad2          => Smad2            ; Cell * MA1(       kSmad2Dephos,   pSmad2           );
+        //TGFb module
+        R1:                 => TGFbR            ; Cell * kTGFbRProd                             ;
+        R2: TGFbR           =>                  ; Cell * kTGFbRDeg      *TGFbR                  ;     
+        R4: TGFbR_a         => TGFbR            ; Cell * kTGFbOff       *TGFbR_a                ;
+        R5: Smad2           => pSmad2           ; Cell * kSmad2Phos     *Smad2      *TGFbR_a    ;
+        R6: pSmad2          => Smad2            ; Cell * kSmad2Dephos   *pSmad2                 ;
         
-        // MEK module                   //
-        R7_1:   Mek      => pMek     ;   Cell * MA1Mod( kMekPhosByPI3K,     Mek,    pPI3K   );
-        R7_2:   Mek      => pMek     ;   Cell * MA1Mod( kMekPhosByTGFbR_a,  Mek,    TGFbR_a );
-        R7_3:   Mek      => pMek     ;   Cell * MA1Mod( kMekPhosByIGFR,     Mek,    pGFR    );
-        R8_1:   pMek     => Mek      ;   Cell * MA1Mod( kMekDephosByAkt,    pMek,   pAkt    );
-        R8_2:   pMek     => Mek      ;   Cell * MA1Mod( kMekDephosByAZD,    pMek,   AZD     ); 
-        R9 :    Erk      => pErk     ;   Cell * MA1Mod( kErkPhosByMek,      Erk,    pMek    );
-        R10:    pErk     => Erk      ;   Cell * MA1(    kErkDephos,         pErk)
+        //MEK module                   //
+        R7_1:   Mek      => pMek     ;   Cell * kMekPhosByPI3K      *Mek    *pPI3K   ;
+        R7_2:   Mek      => pMek     ;   Cell * kMekPhosByTGFbR_a   *Mek    *TGFbR_a ;
+        R7_3:   Mek      => pMek     ;   Cell * kMekPhosByIGFR      *Mek    *pGFR    ;
+        R8_1:   pMek     => Mek      ;   Cell * kMekDephosByAkt     *pMek   *pAkt    ;
+        R8_2:   pMek     => Mek      ;   Cell * kMekDephosByAZD     *pMek   *AZD     ; 
+        R9 :    Erk      => pErk     ;   Cell * kErkPhosByMek       *Erk    *pMek    ;
+        R10:    pErk     => Erk      ;   Cell * kErkDephos          *pErk
         
         
         //PI3K Module
-        //R11:        GFR    => pGFR      ;   Cell *(kIGFRPhos_kcat  * GrowthFactors * GFR /(kIGFRPhos_km^kIGFRPhos_h + GFR^kIGFRPhos_h))
-        R11:        GFR    => pGFR      ;   Cell * HillWithKcat(kIGFRPhos_kcat, GrowthFactors, GFR, kIGFRPhos_km, kIGFRPhos_h )
-        R12:        pGFR   => GFR       ;   Cell * MA1(     kIGFRDephos,        pGFR                    );
-        R13_1:      PI3K    => pPI3K    ;   Cell * MA1Mod(  kPI3KPhosByIGFR,    PI3K,       pGFR        );
-        R13_2:      PI3K    => pPI3K    ;   Cell * MA1Mod(  kPI3KPhosByMek,     PI3K,       pMek        );
-        R14_1:      pPI3K   => PI3K     ;   Cell * MA1Mod(  kPI3KDephosByS6K,   pPI3K,      pS6K        );
-        R14_2:      pPI3K   => PI3K     ;   Cell * MA1Mod(  kPI3KDephosByErk,   pPI3K,      pErk        );
-        R15:        Akt     => pAkt     ;   Cell * MA1Mod(  kAktPhos,           Akt,        pPI3K       );
-        R16:        pAkt    => Akt      ;   Cell * MA1Mod(  kAktDephos,         pAkt,       MK2206      );
-        R17_1:      mTORC1  => pmTORC1  ;   Cell * MA1Mod(  kmTORC1PhosByAkt,   mTORC1,     pAkt        );
-        R17_2:      mTORC1  => pmTORC1  ;   Cell * MA1Mod(  kmTORC1PhosByErk,   mTORC1,     pErk        );
-        R18_1:      pmTORC1 => mTORC1   ;   Cell * MA1Mod(  kmTORC1DephosByEv,  pmTORC1,    Everolimus  );
-        R18_2:      pmTORC1 => mTORC1   ;   Cell * MA1(     kmTORC1Dephos,      pmTORC1                 );
-        R19_1:      S6K     => pS6K     ;   Cell * MA1Mod(  kS6KPhosBymTORC1,   S6K,        pmTORC1     );
-        R19_2:      S6K     => pS6K     ;   Cell * MA1Mod(  kS6KPhosByErk,      S6K,        pErk        );
-        R20:        pS6K    => S6K      ;   Cell * MA1(     kS6KDephos,         pS6K                    );
+        R11:        GFR     => pGFR     ;   Cell *(kIGFRPhos_kcat       * GrowthFactors * GFR /(kIGFRPhos_km^kIGFRPhos_h + GFR^kIGFRPhos_h))
+        R12:        pGFR    => GFR      ;   Cell *  kIGFRDephos         *pGFR                    ;
+        R13_1:      PI3K    => pPI3K    ;   Cell *  kPI3KPhosByIGFR     *PI3K       *pGFR        ;
+        R13_2:      PI3K    => pPI3K    ;   Cell *  kPI3KPhosByMek      *PI3K       *pMek        ;
+        R14_1:      pPI3K   => PI3K     ;   Cell *  kPI3KDephosByS6K    *pPI3K      *pS6K        ;
+        R14_2:      pPI3K   => PI3K     ;   Cell *  kPI3KDephosByErk    *pPI3K      *pErk        ;
+        R15:        Akt     => pAkt     ;   Cell *  kAktPhos            *Akt        *pPI3K       ;
+        R16:        pAkt    => Akt      ;   Cell *  kAktDephos          *pAkt       *MK2206      ;
+        R17_1:      mTORC1  => pmTORC1  ;   Cell *  kmTORC1PhosByAkt    *mTORC1     *pAkt        ;
+        R17_2:      mTORC1  => pmTORC1  ;   Cell *  kmTORC1PhosByErk    *mTORC1     *pErk        ;
+        R18_1:      pmTORC1 => mTORC1   ;   Cell *  kmTORC1DephosByEv   *pmTORC1    *Everolimus  ;
+        R18_2:      pmTORC1 => mTORC1   ;   Cell *  kmTORC1Dephos       *pmTORC1                 ;
+        R19_1:      S6K     => pS6K     ;   Cell *  kS6KPhosBymTORC1    *S6K        *pmTORC1     ;
+        R19_2:      S6K     => pS6K     ;   Cell *  kS6KPhosByErk       *S6K        *pErk        ;
+        R20:        pS6K    => S6K      ;   Cell *  kS6KDephos          *pS6K                    ;
         
         kTGFbOn_kcat        = 50
-        kTGFbOn_halve       = 35
+        kTGFbOn_km          = 35
         kTGFbOn_h           = 2
         kTGFbRProd          = 0.1
         kTGFbRDeg           = 0.001
-        //kTGFbOn             = 0.01
         kTGFbOff            = 0.01
         kSmad2Phos          = 0.01
         kSmad2Dephos        = 0.01
@@ -162,10 +102,11 @@ def cross_talk_model_antstr():
         kErkPhosByMek       = 0.01    
         kErkDephos          = 0.01
         
-        kIGFRPhos_kcat      = 100
-        kIGFRPhos_km        = 2500
+        kIGFRPhos_kcat      = 10
+        kIGFRPhos_km        = 25
         kIGFRPhos_h         = 2
-        kIGFRDephos         = 0.01
+        kIGFRDephos         = 0.1
+        
         kPI3KPhosByIGFR     = 0.01    
         kPI3KPhosByMek      = 0.01    
         kPI3KDephosByS6K    = 0.01        
@@ -180,7 +121,7 @@ def cross_talk_model_antstr():
         kS6KPhosByErk       = 0.001    
         kS6KDephos          = 0.01
     
-        TGFb                    = 1
+        TGFb                    = 0
         AZD                     = 0
         GrowthFactors           = 100
         MK2206                  = 0
@@ -194,8 +135,8 @@ def cross_talk_model_antstr():
         pMek                    = 0
         Erk                     = 100
         pErk                    = 0
-        GFR                    = 86.221
-        pGFR                   = 13.779
+        GFR                     = 100
+        pGFR                    = 0
         PI3K                    = 100
         pPI3K                   = 0
         Akt                     = 100
@@ -319,8 +260,8 @@ if __name__ == '__main__':
 
 
     # dose_response_for_growth_factors()
-    mod = load_model_with_pyco(functions_antstr()+cross_talk_model_antstr(), copasi_filename)
-    mod.open()
+    # mod = load_model_with_pyco(functions_antstr()+cross_talk_model_antstr(), copasi_filename)
+    # mod.open()
 
     # mod = te.loada(functions_antstr() + cross_talk_model_antstr())
     # odes = te.utils.misc.getODEsFromModel(mod)
@@ -328,15 +269,20 @@ if __name__ == '__main__':
 
     # sbml = te.antimonyToSBML(functions_antstr() + cross_talk_model_antstr())
     # print(sbml)
-    # i = 'GFR'
-    # j = 'pGFR'
+    i = 'GFR'
+    j = 'pGFR'
     # pmid_for_IGF_data = 'PMID: 26217307'
-    # fig = dose_response(cross_talk_model_antstr(), 'GrowthFactors', 0.1, 1000, 100, [i, j])
-    # plt.title('Dose Response of {} and \n{} to GrowthFactors'.format(i, j))
-    # fname = os.path.join(graphs_directory, 'GrowthFactorsDoseResponse{}.png'.format(i))
-    # fig.savefig(fname, dpi=150, bbox_inches='tight')
-    # plt.show()
+    fig = dose_response(cross_talk_model_antstr(), 'GrowthFactors', 0.1, 1000, 10, [i, j])
+    plt.title('Dose Response of {} and \n{} to GrowthFactors'.format(i, j))
+    fname = os.path.join(graphs_directory, 'GrowthFactorsDoseResponse{}.png'.format(i))
+    fig.savefig(fname, dpi=150, bbox_inches='tight')
+    plt.show()
 
+    # print(cross_talk_model_antstr())
+    # import re
+
+    # r = re.sub('MA1Mod\( (.*), *(.*), *(.*)\)', '\\1*\\2*\\3', cross_talk_model_antstr())
+    # print(r)
     # print(te.getVersionInfo())
     # model_string,
     # scan_parameter,
